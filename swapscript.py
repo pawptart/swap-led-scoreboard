40ba48d8a16370bf7e558b123860bd2bcc68ec63
Tyler Porter <46663285+pawptart@users.noreply.github.com>
2020-03-23 10:26:28 -0400

    Update script to rotate automatically

--- swapscript.py
+++ swapscript.py
@@ -1,51 +1,24 @@
-from gpiozero import Button
-from signal import pause
 import time
 from subprocess import check_call
 from subprocess import call
 import os
 
 
-killallcmd = "sudo supervisorctl stop all" # command to kill all supervisor run processes
-boardnames = ["MLB","NHL"] #commands to run various boards/processes
-timetohold = 5 # time to hold button before executing shutdown
-buttonnumber = 3 # number of the button (actual pin) being used for the button
-x = 0 # state tracking variable
+board_names = ["MLB","NHL"] #commands to run various boards/processes
+sleep_time  = 120 # seconds
 
-Button.was_held = False
-
-def held(btn):
-	btn.was_held = True
-	print("Button hold detected.  Shutting down.")
-	time.sleep(1)
-	print("-----SHUTDOWN COMMAND HERE-----")
-	os.system("sudo shutdown -h now")
-
-def released(btn):
-	global x
-	if not btn.was_held:
-		pressed()
-	time.sleep(1)
-	if x == len(boardnames): # resets counter
-		x = 0
+def run(board_state):
+	time.sleep(sleep_time)
+	if board_state == len(board_names): # resets counter
+		board_state = 0
 	y = 0
-	while y < len(boardnames):  # iterates through all boards except selected and kills them
-		if y != x:
-			execcommand = "sudo supervisorctl stop %s" % boardnames[y]
+	while y < len(board_names):  # iterates through all boards except selected and kills them
+		if y != board_state:
+			execcommand = "sudo supervisorctl stop %s" % board_names[y]
 			os.system(execcommand)
 		y = y + 1
-	execcommand = "sudo supervisorctl start %s" % boardnames[x]
+	execcommand = "sudo supervisorctl start %s" % board_names[x]
 	os.system(execcommand)  # runs the board
-	x = x + 1
-	btn.was_held = False
-
-def pressed():
-	print("Button press detected.  Swapping sports.")
-
-btn = Button(buttonnumber, hold_time=timetohold)
-
-btn.when_held = held
-btn.when_released = released
-
-pause()
-
+	board_state += 1
+	
+	run(board_state)
